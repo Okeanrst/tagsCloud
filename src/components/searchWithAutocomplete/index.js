22475d153a1cc5fc1a489e9b025c5975fd5fe9f2
e.shamray <e.shamray.oss@gmail.com>
2021-11-25 12:04:07 +0200

    update react to 17.0.2; update react-scripts to 4

--- src/components/searchWithAutocomplete/index.js
+++ src/components/searchWithAutocomplete/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import deburr from 'lodash/deburr';
+import deburr from 'lodash.deburr';
 import Downshift from 'downshift';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
