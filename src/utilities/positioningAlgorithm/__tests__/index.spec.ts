830447f732a4f159713287c49308668a70c6ec8f
e.shamray <e.shamray@ossystem.ua>
2021-12-25 09:46:59 +0200

    add test on rect areas intersection in final result

--- src/utilities/positioningAlgorithm/__tests__/index.spec.ts
+++ src/utilities/positioningAlgorithm/__tests__/index.spec.ts
@@ -1,9 +1,11 @@
 import { prepareData } from 'utilities/tagsCloud/tagsCloud';
-import { calcTagsPositions } from '../calcTagsPositions';
 import { prepareRectAreasMaps } from 'utilities/prepareRectAreasMaps';
+import { calcTagsPositions } from '../calcTagsPositions';
+import { SceneMap } from '../sceneMap';
 import entryData from './entryData.json';
 import preparedData from './preparedData.json';
 import tagsRectAreasMaps from './rectAreasMaps.json';
+import fullSizeFilledRectAreasMaps from './fullSizeFilledRectAreasMaps.json';
 import preparedDataWithPositions from './preparedDataWithPositions.json';
 
 jest.mock('utilities/common/getRandomRGBColor', () => {
@@ -31,15 +33,40 @@ describe('positioningAlgorithm tests', () => {
   });
 
   describe('prepareRectAreasMaps tests', () => {
-    it(`should return correct result`, async () => {
-      await prepareRectAreasMaps(preparedData, 2);
+    it(`should return array with correct value (right shape)`, async () => {
+      const tagsRectAreasMaps = await prepareRectAreasMaps(preparedData, 2);
+      expect(tagsRectAreasMaps).toStrictEqual(preparedData.map(({id}) => ({id, map: null, mapMeta: null})));
     });
   });
 
   describe('calcTagsPositions tests', () => {
-    it(`calcTagsPositions todo`, async () => {
+    it(`return result is equal to snapshot`, async () => {
       const tagsPositions = await calcTagsPositions(preparedData, tagsRectAreasMaps);
       expect(tagsPositions).toStrictEqual(preparedDataWithPositions);
+    });
+
+    it(`no intersection`, async () => {
+      const tagsPositions = await calcTagsPositions(preparedData, fullSizeFilledRectAreasMaps);
+
+      let intersection = 0;
+      const map = new Map();
+      tagsPositions.forEach(({rectTop, rectBottom, rectRight, rectLeft}) => {
+        const firstRow = SceneMap.calcPrevPositionFromPositionEdge(rectTop);
+        const lastRow = SceneMap.calcNextPositionFromEdge(rectBottom);
+        const firstCol = SceneMap.calcNextPositionFromEdge(rectLeft);
+        const lastCol = SceneMap.calcPrevPositionFromPositionEdge(rectRight);
+        for (let row = firstRow; row >= lastRow; row--) {
+          for (let col = firstCol; col <= lastCol; col++) {
+            const key = `${row},${col}`;
+            if (map.has(key)) {
+              intersection++;
+            } else {
+              map.set(key, true);
+            }
+          }
+        }
+      });
+      expect(intersection).toBe(0);
     });
   });
 });
