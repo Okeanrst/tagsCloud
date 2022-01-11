842af707fe5fb70d1ca0b3d9f5cbe07dc475729e
e.shamray <e.shamray@ossystem.ua>
2022-01-11 09:19:51 +0200

    add test for getRandomRGBColor

--- src/utilities/common/getRandomRGBColor.ts
+++ src/utilities/common/getRandomRGBColor.ts
@@ -2,5 +2,5 @@ export function getRandomRGBColor(): string {
   let r = Math.round(Math.random() * 0xff);
   let g = Math.round(Math.random() * 0xff);
   let b = Math.round(Math.random() * 0xff);
-  return `rgb(${r}, ${g}, ${b})`;
+  return `rgb(${r},${g},${b})`;
 }
