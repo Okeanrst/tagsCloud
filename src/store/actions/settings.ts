d2320c7f9e6bab1e7e959c590911d602278f1313
e.shamray <e.shamray@ossystem.ua>
2022-01-16 22:24:10 +0200

    add resetting rect areas maps on sceneMapResolution is changed

--- src/store/actions/settings.ts
+++ src/store/actions/settings.ts
@@ -5,10 +5,13 @@ import * as actionTypes from './actionTypes';
 
 export function updateSettings(data: Partial<RootStateT['settings']>) {
   return (dispatch: AppDispatchT, getState: GetStateT) => {
-    const { settings: { fontFamily } } = getState();
+    const { settings: { fontFamily, sceneMapResolution } } = getState();
     batch(() => {
       if ('fontFamily' in data && fontFamily !== data.fontFamily) {
         dispatch(createAction(actionTypes.FONT_LOAD_RESET));
+      }
+      if (('fontFamily' in data && fontFamily !== data.fontFamily)
+        || ('sceneMapResolution' in data && sceneMapResolution !== data.sceneMapResolution)) {
         dispatch(createAction(actionTypes.RECT_AREAS_MAPS_RESET));
       }
       dispatch(createAction(actionTypes.SETTINGS_UPDATE, data));
