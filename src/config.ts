b025b1060d3436b54166999e93d006924a32e7a6
Okeanrst <okeanrst@gmail.com>
2025-07-20 14:42:01 +0300

    remove packageManager; add render-build.sh

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,10 @@
 const {
   REACT_APP_SENTRY_DSN: SENTRY_DSN,
   NODE_ENV,
+  REACT_APP_RENDER_GIT_COMMIT,
 } = process.env;
+
+console.log('build commit:', REACT_APP_RENDER_GIT_COMMIT);
 
 export const config = {
   SENTRY_DSN,
