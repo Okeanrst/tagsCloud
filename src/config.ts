39dba4f2df1176c83a3506dbe9adeb3a97d46b95
Okeanrst <okeanrst@gmail.com>
2025-07-27 15:09:51 +0300

    suppress eslint warning

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 const { REACT_APP_SENTRY_DSN: SENTRY_DSN, NODE_ENV, REACT_APP_RENDER_GIT_COMMIT } = process.env;
 
+// eslint-disable-next-line no-console
 console.log('build commit:', REACT_APP_RENDER_GIT_COMMIT);
 
 export const config = {
