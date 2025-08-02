d7d12c525fe464044866d959d71043328e90d0bc
Okeanrst <okeanrst@gmail.com>
2025-08-02 23:18:26 +0300

    update simplest service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,3 +7,8 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   console.log('service worker activated');
 });
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+  // An empty fetch handler is sufficient to meet the PWA installability criteria.
+});
