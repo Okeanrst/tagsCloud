704bd798d774abb38b2b632f04723d4bfb8d065c
Okeanrst <okeanrst@gmail.com>
2025-08-07 21:30:40 +0300

    unregister serviceWorker

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -13,7 +13,7 @@ export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready
       .then((registration) => {
-        registration.unregister();
+        return registration.unregister();
       })
       .catch((error) => {
         console.error(error.message);
