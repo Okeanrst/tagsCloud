0db7798ab107dab50f818ec9f689086ad9c1192c
Okeanrst <okeanrst@gmail.com>
2025-08-07 21:23:53 +0300

    Revert "remove service-worker"
    This reverts commit c5fd56872d281a365f86a8f289af22efb327b834.

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import packageJson from '../package.json';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import { config } from './config';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 Sentry.init({
   dsn: config.SENTRY_DSN,
@@ -30,3 +31,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
