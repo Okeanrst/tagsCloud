a09be2006c7be95fe3c60bfb389fbd7cde442f6b
Okeanrst <okeanrst@gmail.com>
2026-04-19 08:58:06 +0300

    improve omit typing

--- src/utilities/helpers/omit.ts
+++ src/utilities/helpers/omit.ts
@@ -1,12 +1,12 @@
-export function omit<T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K>;
-export function omit<T, K extends keyof T>(obj: T, keys: K[]): Omit<T, K>;
-export function omit(obj: any, ...keysOrArr: any[]): any {
-  const keys = Array.isArray(keysOrArr[0]) ? keysOrArr[0] : keysOrArr;
-  const result: any = {};
+export function omit<T extends object, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K>;
+export function omit<T extends object, K extends keyof T>(obj: T, keys: readonly K[]): Omit<T, K>;
+export function omit<T extends object, K extends keyof T>(obj: T, ...keysOrArr: [readonly K[]] | K[]): Omit<T, K> {
+  const keys = (Array.isArray(keysOrArr[0]) ? keysOrArr[0] : keysOrArr) as readonly K[];
+  const result = {} as Omit<T, K>;
 
-  for (const key of Object.keys(obj) as Array<keyof typeof obj>) {
-    if (!keys.includes(key)) {
-      result[key] = obj[key];
+  for (const key of Object.keys(obj) as Array<keyof T>) {
+    if (!keys.includes(key as K)) {
+      Object.assign(result, { [key]: obj[key] });
     }
   }
 
